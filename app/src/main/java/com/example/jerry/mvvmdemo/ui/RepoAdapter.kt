516197d1d46e5cae9698a71ff3b4b550e6669db3
CJ-Yang <cj.yang0225@gmail.com>
2020-06-07 03:10:41 +0800

    創建 RecyclerView, Adapter and Data Binding

--- app/src/main/java/com/example/jerry/mvvmdemo/ui/RepoAdapter.kt
+++ app/src/main/java/com/example/jerry/mvvmdemo/ui/RepoAdapter.kt
@@ -1,19 +1,24 @@
 package com.example.jerry.mvvmdemo.ui
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.jerry.mvvmdemo.data.model.Repo
+import com.example.jerry.mvvmdemo.databinding.RepoItemBinding
 
 
-data class RepoAdapter(var items: ArrayList<Repo>) : RecyclerView.Adapter<RepoAdapter.RepoViewHolder>() {
+internal data class RepoAdapter(var items: ArrayList<Repo>) : RecyclerView.Adapter<RepoAdapter.RepoViewHolder>() {
 
-    class RepoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
+    // 設定 binding 初始值
+    internal class RepoViewHolder(internal val binding: RepoItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = RepoItemBinding.inflate(layoutInflater, parent, false)
+
+        return RepoViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -21,12 +26,56 @@ data class RepoAdapter(var items: ArrayList<Repo>) : RecyclerView.Adapter<RepoAd
     }
 
     override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        // In Java: Repo repo = items.get(position);
+        // id & name are never used, so replace with "_"
+        val (_, _, fullName, description, stars, owner) = items[position]
+
+        Glide.with(holder.itemView.context)
+                .load(owner.avatarUrl)
+                .into(holder.binding.ownerAvatar)
+        holder.binding.name.text = fullName
+        holder.binding.desc.text = description
+        holder.binding.stars.text = stars.toString()
     }
 
     fun clearItems() {
         val size: Int = this.items.size
         this.items.clear()
         notifyItemRangeRemoved(0, size)
+    }
+
+    fun swapItems(newItems: List<Repo>?) {
+        // DiffUtil.DiffResult 比對數據後，返回差異結果
+        val result: DiffUtil.DiffResult = DiffUtil.calculateDiff(RepoDiffCallback(this.items, newItems))
+        if (newItems != null) {
+            this.items.clear()
+            this.items.addAll(newItems)
+            result.dispatchUpdatesTo(this)
+        }
+    }
+
+    open class RepoDiffCallback(private val oldList: List<Repo>?, private val newList: List<Repo>?) : DiffUtil.Callback() {
+
+        override fun getOldListSize(): Int {
+            return oldList?.size ?: 0
+        }
+
+        override fun getNewListSize(): Int {
+            return newList?.size ?: 0
+        }
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            if (oldList.isNullOrEmpty() || newList.isNullOrEmpty()) return false
+            val oldId = oldList[oldItemPosition].id
+            val newId = newList[newItemPosition].id
+            return oldId == newId
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            if (oldList.isNullOrEmpty() || newList.isNullOrEmpty()) return false
+            val oldRepo = oldList[oldItemPosition]
+            val newRepo = newList[newItemPosition]
+            return oldRepo === newRepo
+        }
     }
 }
--- app/src/main/java/com/example/jerry/mvvmdemo/ui/RepoFragment.kt
+++ app/src/main/java/com/example/jerry/mvvmdemo/ui/RepoFragment.kt
@@ -1,14 +1,17 @@
 package com.example.jerry.mvvmdemo.ui
 
 import android.app.Activity
-import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jerry.mvvmdemo.data.model.Repo
 import com.example.jerry.mvvmdemo.databinding.FragmentRepoBinding
 import com.example.jerry.mvvmdemo.viewmodel.GithubViewModelFactory
 import kotlinx.android.synthetic.main.fragment_repo.*
@@ -24,43 +27,55 @@ private const val TAG = "Repo"
  */
 class RepoFragment : Fragment() {
     private lateinit var binding: FragmentRepoBinding
-    val repoAdapter = RepoAdapter(ArrayList())
-    private val factory: GithubViewModelFactory = GithubViewModelFactory()
+    private val repoAdapter = RepoAdapter(ArrayList<Repo>())
 
+    private val factory: GithubViewModelFactory = GithubViewModelFactory()
     private lateinit var viewModel: RepoViewModel
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-// error       viewModel = ViewModelProvider.of()
-
-
-        btn_search.setOnClickListener { doSearch() }
+    private fun newInstance(): RepoFragment? {
+        return RepoFragment()
     }
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        viewModel = ViewModelProvider(this, factory).get(RepoViewModel::class.java)
+
+        viewModel.getRepos().observe(this, Observer<List<Repo>> { repos ->
+            repoAdapter.swapItems(repos)
+        })
 
         binding = FragmentRepoBinding.inflate(inflater, container, false)
 
         return binding.root
     }
 
-//    companion object {
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//                RepoFragment().apply {
-//                    arguments = Bundle().apply {
-//
-//                    }
-//                }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    fun View.hideKeyboard(inputMethodManager: InputMethodManager) {
-        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+        binding.btnSearch.setOnClickListener { doSearch() }
+        binding.editQuery.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                doSearch()
+                return@OnKeyListener true
+            }
+            false
+        })
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = repoAdapter
+
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+//    fun View.hideKeyboard(inputMethodManager: InputMethodManager) {
+//        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+//    }
 
     private fun doSearch() {
         val query: String = binding.editQuery.text.toString()
@@ -71,7 +86,7 @@ class RepoFragment : Fragment() {
         viewModel.searchRepo(query)
 
         // 關閉軟鍵盤
-      val imm = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager?
+        val imm = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager?
         imm?.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
     }
 }
